eaf233560bd8e800a419e5463b7bc2d2fbeaeb0e
Max <shpax1@gmail.com>
2018-06-03 12:25:11 +0300

    rednder update

--- jsfile.js
+++ jsfile.js
@@ -3,25 +3,23 @@ window.addEventListener("load", function(){
  renderToDos();
 });
 
-const TODO = {
-  text: "on pokazyval pisun",
-  done: false
-};
-
 function createToDo(text) {
   return {text, done: false};
 };
 
 const toDoStore = [
-  createToDo("testToDo1"),
-  createToDo("testToDo2")
-];
 
+  ];
 
 function renderToDos() {
-  const toDosContainer = document.getElementById("addButton");
+  const toDosContainer = document.getElementById("toDosList");
   const html = toDoStore.map(function (toDo) {
     return "<li class=\"list-group-item\">" + toDo.text + "</li>";
   }).join("\n");
   toDosContainer.innerHTML = html;
 };
+
+
+
+
+
